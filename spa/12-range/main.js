09607a62ec87a337dd65fd8e92569c5959e73fbb
housuhong2017 <379126913@qq.com>
2019-03-26 15:47:45 +0800

    add 12-range

---
+++ spa/12-range/main.js
@@ -0,0 +1,10 @@
+$(function(){
+  var $range = $('input[type="range"]');
+  var $txtAge = $('#age');
+
+  $range.change(function(){
+    console.log($range.val())
+
+    $txtAge.html($range.val());
+  })
+  });
